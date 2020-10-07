180a85d43d182533f76e036e66bc147bf66bdc48
nguyenkhachieu <nguyenkhachieu0508@gmail.com>
2020-10-07 20:24:29 +0700

    first commit project

--- App.js
+++ App.js
@@ -1,12 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+  Button,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  ScrollView,
+  FlatList,
+  TouchableOpacity
+} from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState('Hieu');
+  const [people, setPeople] = useState([
+    {name: 'kaka', id: '1'},
+    {name: 'ronaldo', id: '2'},
+    {name: 'ronaldo1', id: '3'},
+    {name: 'ronaldo2', id: '4'},
+    {name: 'ronaldo3', id: '5'}
+  ])
+
+
+  const onChangeName = () => {
+    setName('haha')
+  }
+  const pressHandler = (id) => {
+    console.log(id)
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* <ScrollView> */}
+      <View style={styles.header}>
+        <Text style={styles.boldText}>My name is {name}</Text>
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button title="update state" onPress={onChangeName}></Button>
+      </View>
+      <View>
+        <Text>Enter Name</Text>
+        <TextInput style={styles.input} onChangeText={(val) => setName(val)} />
+      </View>
+      {/* {
+        people && people.map(item => {
+          return (
+            <View key={item.id}>
+              <Text style={styles.item}>{item.name}</Text>
+            </View>
+          )
+        })
+      } */}
+      {/* </ScrollView> */}
+      {/* ScrollView and FlatList the same */}
+      <FlatList
+        numColumns={2}
+        keyExtractor={(item) => item.id}
+        data={people}
+        horizontal={false}
+        renderItem={({ item }) => (
+          <TouchableOpacity onPress={() => pressHandler(item.id)}>
+            <Text style={styles.item}>{item.name}</Text>
+          </TouchableOpacity>
+        )}
+      />
     </View>
   );
 }
@@ -17,5 +72,30 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 20
   },
+  header: {
+    backgroundColor: 'pink',
+    padding: 20,
+  },
+  boldText: {
+    fontWeight: 'bold',
+  },
+  buttonContainer: {
+    marginTop: 20,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#777',
+    padding: 8,
+    margin: 10,
+    width: 200
+  },
+  item: {
+    marginTop: 24,
+    backgroundColor: 'pink',
+    fontSize: 24,
+    padding: 30,
+    margin: 10
+  }
 });
